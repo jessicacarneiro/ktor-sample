704ed08bf262b744e2faed68afa72f0f6fb82ba0
Jéssica Carneiro <jessicaccarneiro91@gmail.com>
2023-02-02 19:15:56 -0300

    Get token

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,50 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
+import io.ktor.util.*
 
+@OptIn(InternalAPI::class)
 fun Application.configureRouting() {
 
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
+    val client = HttpClient(CIO) {
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
         }
     }
+
+    routing {
+        get("/token") {
+            val tokenResponse = client.post("https://accounts.spotify.com/api/token") {
+                headers {
+                    append(HttpHeaders.Authorization, "Basic ${generateKey()}")
+                    append(HttpHeaders.ContentType, ContentType.Application.FormUrlEncoded)
+                }
+                body = FormDataContent(Parameters.build {
+                    append("client_id", System.getenv("CLIENT_ID"))
+                    append("client_secret", System.getenv("CLIENT_SECRET"))
+                    append("grant_type", "client_credentials")
+                })
+            }
+
+            call.respondText(tokenResponse.body(), ContentType.Application.Json)
+        }
+    }
+}
+
+
+fun generateKey(): String {
+    val clientId = System.getenv("CLIENT_ID")
+    val clientSecret = System.getenv("CLIENT_SECRET")
+
+    return "$clientId:$clientSecret".encodeBase64()
 }
